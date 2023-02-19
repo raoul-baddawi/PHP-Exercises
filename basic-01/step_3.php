da09c475a68f4580b099f5703fc7c2e927a0afa3
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-02-19 17:29:04 +0200

    Step_3

---
+++ basic-01/step_3.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Raoul baddawi";
+    $email = "raoulbaddawi1@gmail.com";
+    $height = 3;
+    $width = 5;
+    $area = $height * $width;
+    echo "My name is $name, and my email is $email<br>";
+    echo "The area is: " . $area;
+?>
