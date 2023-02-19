e1e42262fa05a5a1e1179d0e34ac3df6026c6266
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-02-19 17:29:41 +0200

    Step_7

---
+++ basic-01/step_7.php
@@ -0,0 +1,10 @@
+<?php
+    $birth_year = 2001;
+    $current_year = 2023;
+
+    if ($current_year - $birth_year > 18) {
+        echo "Fik tsou2";
+    } else {
+        echo "You're still a kid, go and play GTA";
+    }
+?>
