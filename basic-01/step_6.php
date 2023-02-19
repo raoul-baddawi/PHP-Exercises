6829a7c1313988746e1fea6d2728c479b4b4eec7
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-02-19 17:29:34 +0200

    Step_6

---
+++ basic-01/step_6.php
@@ -0,0 +1,10 @@
+<?php
+    function calculateArea($height, $width) {
+        $area = $height * $width;
+        echo "Area is " . $area;
+    }
+
+    calculateArea(5, 5);
+
+    calculateArea(5, 3);
+?>
